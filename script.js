6a19ea4d197d1225f6e603cfca6d661185e2f34b
erasmusarea <arsakayaosman81@gmail.com>
2026-06-13 22:27:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,57 @@ darkToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     darkToggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
+
+// GALLERY WITH NEXT/PREV
+const galleryItems = document.querySelectorAll(".gallery-item");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+const lightboxClose = document.getElementById("lightboxClose");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+let currentIndex = 0;
+
+// Fotoğrafları liste olarak al
+const images = [
+    "images/1.foto.jpeg",
+    "images/2.foto.jpeg",
+    "images/3.foto.jpeg",
+    "images/4.foto.jpeg",
+    "images/5.foto.jpeg",
+    "images/6.foto.jpeg"
+];
+
+
+// Fotoğraf açma
+galleryItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        currentIndex = index;
+        lightboxImg.src = images[currentIndex];
+        lightbox.style.display = "flex";
+    });
+});
+
+// Sonraki foto
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    lightboxImg.src = images[currentIndex];
+});
+
+// Önceki foto
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    lightboxImg.src = images[currentIndex];
+});
+
+// Kapatma
+lightboxClose.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+// Boş alana tıklayınca kapansın
+lightbox.addEventListener("click", (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
+    }
+});
